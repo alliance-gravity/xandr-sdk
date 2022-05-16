066de1c1dbdfb1a2b23e0157a5eb7e0095feb9d9
Daniel Baud <danielbaudf@gmail.com>
2022-05-16 18:06:16 +0200

    fix(utils): handling all content types containing JSON

--- src/utils.ts
+++ src/utils.ts
@@ -20,7 +20,9 @@ export async function request<ExpectedResponseType> (params: RequestParameters, 
       ? JSON.stringify(params.body) 
       : params.formData ? params.formData : undefined
   });
-  const isJson = response.headers.get('Content-Type') === 'application/json' || response.headers.get('Content-Type') === 'application/appnexus.apd.vauxhall.v1.0+json';
+  const contentType = response.headers.get('Content-Type');
+  const [contentTypeGroup, contentTypeSub] = contentType !== null ? contentType.split('/') : ['', ''];
+  const isJson = contentTypeGroup === 'application' && 'json' in contentTypeSub.split('+');
   const responseBody = await response.text();
   if (response.status === 204) {
     return {} as ExpectedResponseType;
