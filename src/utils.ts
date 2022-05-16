9a65b626f176dbeb9abdf46be1405f2ea2e8e22a
Daniel Baud <danielbaudf@gmail.com>
2022-05-16 14:32:28 +0200

    fix(utils): handles empty response with malformed content type

--- src/utils.ts
+++ src/utils.ts
@@ -29,7 +29,8 @@ export async function request<ExpectedResponseType> (params: RequestParameters, 
     const headers = Object.fromEntries(response.headers.entries());
     if (isJson) {
       const responseJson = JSON.parse(responseBody) as XandrResponse<XandrGeneralError>;
-      throw new XandrError(responseJson.response.error, responseJson.response.error_id, response.status, headers);
+      if ('reponse' in responseJson)
+        throw new XandrError(responseJson.response.error, responseJson.response.error_id, response.status, headers);
     }
     throw new XandrError(responseBody, 'ERROR', response.status, headers);
   }
